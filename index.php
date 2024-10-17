af8ccf95106783a77bb0846c685674d5082c220f
Kira07 <122767231+omros@users.noreply.github.com>
2024-10-17 14:20:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,46 @@ session_start();
 $default_lang = 'bahasa_indonesia';   
    
 if(!$_SESSION['lang']) {   
+    $_SESSION['lang'] = $default_lang;   
+}   
+   
+ if(isset($_GET['lang'])) {   
+     $_SESSION['lang'] = $_GET['lang'];   
+     header("Location: index.php");   
+ }   
+    
+ // masukan file bahasa yang sedang aktif   
+ include $_SESSION['lang'] . '.php';   
+ ?>   
+    
+ <html>   
+     <head>   
+         <title><?php echo $lang_judul; ?></title>   
+     </head>   
+     <body>   
+         <a href="?lang=bahasa_inggris">Bahasa Inggris</a>   
+         <a href="?lang=bahasa_indonesia">Bahasa Indonesia</a>   
+         <nav>   
+             <ul>   
+                 <li><a href="#"><?php echo $lang_menu_home; ?></a></li>   
+                 <li><a href="#"><?php echo $lang_menu_profile; ?></a></li>   
+                 <li><a href="#"><?php echo $lang_menu_contact; ?></a></li>   
+                 <li><a href="tambah_berita.html"><?php echo $lang_menu_add; ?></a></li>   
+             </ul>   
+         </nav>   
+                
+<p>   
+<?php echo "$lang_selamat_datang" ?>   
+</p>   
+    </body>   
+ </html>
+
+ <?php   
+// Mulai session   
+session_start();   
+$default_lang = 'bahasa_indonesia';   
+   
+if(!$_SESSION['lang']) {   
   $_SESSION['lang'] = $default_lang;   
 }   
    
@@ -66,7 +106,6 @@ if(!$_SESSION['lang']) {
      $hasil=mysqli_query($mysqli,$sql);   
      $row=mysqli_fetch_row($hasil);   
      if ($hasil) {   
-    
        do   
           {   
            list ($id_berita, $waktu_berita, $judul_id, $judul_en, $isi_id,$isi_en,$nama_file,$dihapus)=$row;   
